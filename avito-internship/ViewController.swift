a34ef12f3572050f5a6903f9e1612ad057a20106
Yuriy <ura.yakovlev@yandex.ru>
2022-10-18 22:28:23 +0500

    Initial Commit

---
+++ avito-internship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avito-internship
+//
+//  Created by Юрий Яковлев on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
